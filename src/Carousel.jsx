86742f19b2db4f3b6e34329578387591a1d798bf
Bikal <bikaladhikari5@gmail.com>
2024-04-21 18:37:55 +1000

    mapping the array from short list

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import { shortList, list, longList } from "./data";
+import { FaQuoteRight } from "react-icons/fa";
+
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
   const [people, setPeople] = useState(shortList);
-  return <div>Carousel</div>;
+  return (
+    <section className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Carousel;
